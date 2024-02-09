a9b484ee8f84f6c4b5f4fb8097824de302ccccd4
Infinital-X <bosstoxa2000@mail.ru>
2024-02-10 00:05:51 +0200

    3D'

---
+++ Assets/Scripts/Labirinth/LightScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    private bool _isDay;
+    private bool isDay
+    {
+        get => _isDay;
+        set
+        {
+            LabirinthState.isDay = _isDay = value;
+            if (_isDay) SetDayLighting();
+            else SetNightLighting();
+        }
+    }
+    private Light lightComponent;
+
+    void Start()
+    {
+        lightComponent = GetComponent<Light>();
+        isDay = true;
+    }
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.N))
+        {
+            isDay = !isDay;
+        }
+
+        if (Input.GetKey(KeyCode.Equals) && lightComponent.intensity < 1f)
+        {
+            float intensity = lightComponent.intensity + 0.01f;
+            if (intensity >= 1f) lightComponent.intensity = 1f;
+            else lightComponent.intensity = intensity;
+        }
+        if (Input.GetKey(KeyCode.Minus) && lightComponent.intensity > 0.01f)
+        {
+            float intensity = lightComponent.intensity - 0.01f;
+            if (intensity <= 0.01f) lightComponent.intensity = 0.01f;
+            else lightComponent.intensity = intensity;
+        }
+    }
+
+    private void SetDayLighting()
+    {
+        lightComponent.intensity = 1f;
+    }
+    private void SetNightLighting()
+    {
+        lightComponent.intensity = 0.1f;
+    }
+}
